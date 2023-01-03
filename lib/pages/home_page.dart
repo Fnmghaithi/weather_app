ddfb09ae12d8b99891f7839948739952348ec1a4
Faisal <fnmghaithi@gmail.com>
2023-01-03 17:42:20 +0400

    second commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'there is no weather 😔 start',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              'searching now 🔍',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
