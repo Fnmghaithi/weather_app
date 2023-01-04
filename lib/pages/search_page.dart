770d67aedaaa38904aadcac34490b47cc59956fc
Faisal <fnmghaithi@gmail.com>
2023-01-04 19:08:54 +0400

    Added Search TextField

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -9,26 +9,26 @@ class SearchPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Search a City'),
       ),
-      // body: Center(
-      //   child: Padding(
-      //     padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      //     child: TextField(
-      //       decoration: const InputDecoration(
-      //         contentPadding: EdgeInsets.symmetric(
-      //           vertical: 30.0,
-      //           horizontal: 24.0,
-      //         ),
-      //         hintText: 'Enter a city',
-      //         border: OutlineInputBorder(),
-      //         suffixIcon: Icon(Icons.search),
-      //         label: Text('Search'),
-      //       ),
-      //       onSubmitted: (value) {
-      //         cityName = value;
-      //       },
-      //     ),
-      //   ),
-      // ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: TextField(
+            decoration: const InputDecoration(
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 30.0,
+                horizontal: 24.0,
+              ),
+              hintText: 'Enter a city',
+              border: OutlineInputBorder(),
+              suffixIcon: Icon(Icons.search),
+              label: Text('Search'),
+            ),
+            onSubmitted: (value) {
+              cityName = value;
+            },
+          ),
+        ),
+      ),
     );
   }
 }
