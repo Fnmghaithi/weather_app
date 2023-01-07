09188b6291e0684c359797c2f795b2764735876c
Faisal <fnmghaithi@gmail.com>
2023-01-07 17:19:41 +0400

    Changed it to factory constructor.

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,24 +1,27 @@
 class WeatherModel {
-  String? date;
-  double? temp;
-  double? maxTemp;
-  double? minTemp;
-  String? weatherStateName;
+  final String date;
+  final double temp;
+  final double maxTemp;
+  final double minTemp;
+  final String weatherStateName;
 
-  // WeatherModel({
-  //   required this.date,
-  //   required this.temp,
-  //   required this.maxTemp,
-  //   required this.minTemp,
-  //   required this.weatherStateName,
-  // });
+  WeatherModel({
+    required this.date,
+    required this.temp,
+    required this.maxTemp,
+    required this.minTemp,
+    required this.weatherStateName,
+  });
 
-  WeatherModel.fromJson(dynamic data) {
+  factory WeatherModel.fromJson(dynamic data) {
     var jsonData = data['forecast']['forecastday'][0];
-    date = data['location']['localtime'];
-    temp = jsonData['day']['avgtemp_c'];
-    maxTemp = jsonData['day']['maxtemp_c'];
-    minTemp = jsonData['day']['mintemp_c'];
-    weatherStateName = jsonData['day']['condition']['text'];
+
+    return WeatherModel(
+      date: data['location']['localtime'],
+      temp: jsonData['day']['avgtemp_c'],
+      maxTemp: jsonData['day']['maxtemp_c'],
+      minTemp: jsonData['day']['mintemp_c'],
+      weatherStateName: jsonData['day']['condition']['text'],
+    );
   }
 }
