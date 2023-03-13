cab573ba654a1ac14599feabf00e74262ac81220
Faisal <fnmghaithi@gmail.com>
2023-03-13 19:51:35 +0400

    Refactoring code because of Bloc Provider(Cubit).

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_cubit.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_state.dart';
 import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/pages/search_page.dart';
 import 'package:weather_app/providers/weather_provider.dart';
 
-class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
   WeatherModel? weatherData;
   String? cityName;
 
   @override
   Widget build(BuildContext context) {
-    weatherData = Provider.of<WeatherProvider>(context).weatherData;
-    cityName = Provider.of<WeatherProvider>(context).cityName;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather App'),
@@ -36,27 +30,14 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: weatherData == null
-          ? Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'there is no weather 😔 start',
-                    style: TextStyle(
-                      fontSize: 30,
-                    ),
-                  ),
-                  Text(
-                    'searching now 🔍',
-                    style: TextStyle(
-                      fontSize: 30,
-                    ),
-                  )
-                ],
-              ),
-            )
-          : Container(
+      body: BlocBuilder<WeatherCubit, WeatherState>(
+        builder: (context, state) {
+          if (state is WeatherLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is WeatherSuccess) {
+            return Container(
               decoration: BoxDecoration(
                 gradient: LinearGradient(
                   colors: [
@@ -74,14 +55,14 @@ class _HomePageState extends State<HomePage> {
                   const Spacer(flex: 3),
                   Text(
                     cityName!,
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontSize: 32,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                   Text(
                     'updated at: ${weatherData!.date.hour}:${weatherData!.date.minute}',
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontSize: 22,
                     ),
                   ),
@@ -92,7 +73,7 @@ class _HomePageState extends State<HomePage> {
                       Image.asset(weatherData!.getImage()),
                       Text(
                         weatherData!.temp.toInt().toString(),
-                        style: TextStyle(
+                        style: const TextStyle(
                           fontSize: 32,
                           fontWeight: FontWeight.bold,
                         ),
@@ -108,7 +89,7 @@ class _HomePageState extends State<HomePage> {
                   const Spacer(),
                   Text(
                     weatherData?.weatherStateName ?? '',
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontSize: 32,
                       fontWeight: FontWeight.bold,
                     ),
@@ -116,7 +97,115 @@ class _HomePageState extends State<HomePage> {
                   const Spacer(flex: 5),
                 ],
               ),
-            ),
+            );
+          } else if (state is WeatherFailure) {
+            return const Center(
+              child: Text('Something went wrong please try again'),
+            );
+          } else {
+            return Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text(
+                    'there is no weather 😔 start',
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  Text(
+                    'searching now 🔍',
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+        },
+      ),
+      // body: true
+      // ? Center(
+      //     child: Column(
+      //       mainAxisSize: MainAxisSize.min,
+      //       children: [
+      //         Text(
+      //           'there is no weather 😔 start',
+      //           style: TextStyle(
+      //             fontSize: 30,
+      //           ),
+      //         ),
+      //         Text(
+      //           'searching now 🔍',
+      //           style: TextStyle(
+      //             fontSize: 30,
+      //           ),
+      //         )
+      //       ],
+      //     ),
+      //   )
+      // : Container(
+      //     decoration: BoxDecoration(
+      //       gradient: LinearGradient(
+      //         colors: [
+      //           weatherData!.getThemeColor(),
+      //           weatherData!.getThemeColor()[200]!,
+      //           weatherData!.getThemeColor()[100]!,
+      //         ],
+      //         begin: Alignment.topCenter,
+      //         end: Alignment.bottomCenter,
+      //       ),
+      //     ),
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         const Spacer(flex: 3),
+      //         Text(
+      //           cityName!,
+      //           style: TextStyle(
+      //             fontSize: 32,
+      //             fontWeight: FontWeight.bold,
+      //           ),
+      //         ),
+      //         Text(
+      //           'updated at: ${weatherData!.date.hour}:${weatherData!.date.minute}',
+      //           style: TextStyle(
+      //             fontSize: 22,
+      //           ),
+      //         ),
+      //         const Spacer(),
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //           children: [
+      //             Image.asset(weatherData!.getImage()),
+      //             Text(
+      //               weatherData!.temp.toInt().toString(),
+      //               style: TextStyle(
+      //                 fontSize: 32,
+      //                 fontWeight: FontWeight.bold,
+      //               ),
+      //             ),
+      //             Column(
+      //               children: [
+      //                 Text('maxTemp : ${weatherData!.maxTemp.toInt()}'),
+      //                 Text('maxTemp : ${weatherData!.minTemp.toInt()}'),
+      //               ],
+      //             ),
+      //           ],
+      //         ),
+      //         const Spacer(),
+      //         Text(
+      //           weatherData?.weatherStateName ?? '',
+      //           style: TextStyle(
+      //             fontSize: 32,
+      //             fontWeight: FontWeight.bold,
+      //           ),
+      //         ),
+      //         const Spacer(flex: 5),
+      //       ],
+      //     ),
+      //   ),
     );
   }
 }
